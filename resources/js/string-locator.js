5932116f9ec6f261a3ac6935d7adeb166f7e2859
Clorith <Clorith@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-08 13:30:18 +0000

    Fix to avoid console errors when editor area is not discovered
    git-svn-id: https://plugins.svn.wordpress.org/string-locator/trunk@928521 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- resources/js/string-locator.js
+++ resources/js/string-locator.js
@@ -1,27 +1,30 @@
-var theEditor      = document.getElementById( 'code-editor'),
-	editLine       = theEditor.getAttribute( 'data-editor-goto-line'),
-	editorLanguage = theEditor.getAttribute( 'data-editor-language');
+var theEditor = document.getElementById( 'code-editor');
 
-function resizeEditor( editor ) {
-	var setEditorSize  = ( Math.max( document.documentElement.clientHeight, window.innerHeight || 0 ) - 177 );
+if ( theEditor != null ) {
+	var editLine       = theEditor.getAttribute( 'data-editor-goto-line'),
+		editorLanguage = theEditor.getAttribute( 'data-editor-language');
+
+	function resizeEditor( editor ) {
+		var setEditorSize  = ( Math.max( document.documentElement.clientHeight, window.innerHeight || 0 ) - 177 );
 
 
-	editor.setSize( null, parseInt( setEditorSize ) );
+		editor.setSize( null, parseInt( setEditorSize ) );
+	}
+
+	var editor = CodeMirror.fromTextArea( document.getElementById( 'code-editor' ), {
+		lineNumbers: true,
+		mode: {
+			name            : editorLanguage,
+			globalVars      : true
+		},
+		styleActiveLine : true,
+		matchBrackets   : true,
+		indentWithTabs  : true,
+		indentUnit      : 4,
+		theme           : 'twilight'
+	} );
+
+	editor.scrollIntoView( parseInt( editLine ) );
+	editor.setCursor( parseInt( editLine - 1 ), 0 );
+	resizeEditor( editor );
 }
-
-var editor = CodeMirror.fromTextArea( document.getElementById( 'code-editor' ), {
-	lineNumbers: true,
-	mode: {
-		name            : editorLanguage,
-		globalVars      : true
-	},
-	styleActiveLine : true,
-	matchBrackets   : true,
-	indentWithTabs  : true,
-	indentUnit      : 4,
-	theme           : 'twilight'
-} );
-
-editor.scrollIntoView( parseInt( editLine ) );
-editor.setCursor( parseInt( editLine - 1 ), 0 );
-resizeEditor( editor );
